8b7f503a74f5a42037b1e984ee114fdd43249e0c
Faizan Aamer <faizanaryan94@gmail.com>
2018-08-09 14:54:47 +0500

    small fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { UsersComponent } from "./users/users.component";
-import { DetailsComponent } from "./details/details.component";
-import { PostsComponent } from "./posts/posts.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: UsersComponent
   },
   {
-    path: "details/:id",
+    path: 'details/:id',
     component: DetailsComponent
   },
   {
-    path: "posts",
+    path: 'posts',
     component: PostsComponent
   }
 ];
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { Router, NavigationEnd } from "@angular/router";
-import { Observable } from "../../../node_modules/rxjs";
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
-  selector: "app-sidebar",
-  templateUrl: "./sidebar.component.html",
-  styleUrls: ["./sidebar.component.scss"]
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
   currentUrl: string;
